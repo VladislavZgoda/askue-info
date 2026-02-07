4fc81ca38703826540c587197ed2e9ff1cdde93c
VladislavZgoda <www.vlad78@yandex.ru>
2026-02-07 14:44:17 +0300

    Add reverse migration

--- database/migrations/2026_02_07_104507_change_ip_column_in_sim_cards_table.php
+++ database/migrations/2026_02_07_104507_change_ip_column_in_sim_cards_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sim_cards', function (Blueprint $table) {
-            //
+            $table->ipAddress('ip')->nullable(false)->change();
         });
     }
 };
