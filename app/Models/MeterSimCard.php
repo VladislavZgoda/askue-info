b8113446dcc01aad3af19833e9abd83f660ac2a8
VladislavZgoda <www.vlad78@yandex.ru>
2026-02-15 16:41:41 +0300

    Add validation to MeterSimCard pivot

--- app/Models/MeterSimCard.php
+++ app/Models/MeterSimCard.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Meter;
+use App\Models\SimCard;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class MeterSimCard extends Pivot
@@ -15,10 +17,27 @@ class MeterSimCard extends Pivot
     static::creating(function ($pivot)
     {
       $simCard = SimCard::find($pivot->sim_card_id);
+      $newMeter = Meter::find($pivot->meter_id);
 
       if ($simCard && !is_null($simCard->uspd_id))
       {
-        throw new \Exception('Нельзя привязать SimCard к Meter, так как она уже принадлежит Uspd');
+          throw new \Exception('Нельзя привязать SimCard к Meter, так как она уже принадлежит Uspd');
+      }
+
+      $existingMeters = $simCard->meters;
+      $allMeters = $existingMeters->push($newMeter);
+
+      $installationObjectIds = $allMeters->pluck('installation_object_id')
+                                         ->unique();
+
+      if ($installationObjectIds->count() > 1)
+      {
+          throw new \Exception('SimCard может быть привязана только к Meter, принадлежащим одному InstallationObject');
+      }
+
+      if ($installationObjectIds->contains(null))
+      {
+          throw new \Exception('Все Meter должны иметь InstallationObject');
       }
     });
   }
