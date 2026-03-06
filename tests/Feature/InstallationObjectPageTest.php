0d302e1c2a535e798033da318492f15e568b3bea
zgodavg <www.vlad78@yandex.ru>
2026-03-06 15:07:32 +0300

    Create test for InstallationObject page

---
+++ tests/Feature/InstallationObjectPageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\InstallationObject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('renders page with data', function () {
+    InstallationObject::factory()
+        ->hasMeters(2)
+        ->hasUspds(2)
+        ->create();
+
+    $installationObject = InstallationObject::with(['meters', 'uspds'])
+        ->first();
+
+    $page = visit("/installation-objects/$installationObject->id")
+        ->on()
+        ->mobile();
+
+    $page->assertSee($installationObject->name)
+        ->assertSeeLink('Добавить УСПД')
+        ->assertSeeLink('Добавить ПУ')
+        ->assertSeeLink('Назад')
+        ->assertCount('.group\/item-group', 2);
+
+    foreach ($installationObject->meters as $meter) {
+        $page->assertSee($meter->model)
+            ->assertSee($meter->serial_number);
+    }
+
+    foreach ($installationObject->uspds as $uspd) {
+        $page->assertSee($uspd->model)
+            ->assertSee($uspd->serial_number);
+    }
+});
