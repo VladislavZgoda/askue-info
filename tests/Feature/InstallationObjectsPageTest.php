c5b6f24d86f229d1ce0af8c00a923654d0cafd51
zgodavg <www.vlad78@yandex.ru>
2026-03-06 11:02:24 +0300

    Create tests for Installation Objects page

---
+++ tests/Feature/InstallationObjectsPageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\InstallationObject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('shows the list of installation objects', function () {
+    $installationObjects = InstallationObject::factory()
+        ->count(3)
+        ->create();
+
+    $page = visit('/installation-objects')
+        ->on()
+        ->mobile();
+
+    $page->assertPresent('.group\/item-group')
+        ->assertCount('.group\/item', 3);
+
+    foreach ($installationObjects as $object) {
+        $page->assertSeeLink($object->name)
+            ->assertSee($object->address);
+    }
+});
+
+it('navigates to /installation-objects/id after clicking on an item in the list', function () {
+    $installationObject = InstallationObject::factory()->create();
+
+    $page = visit('/installation-objects')
+        ->on()
+        ->mobile();
+
+    $page->click($installationObject->name)
+        ->assertPathIs("/installation-objects/$installationObject->id")
+        ->assertSee($installationObject->name);
+});
