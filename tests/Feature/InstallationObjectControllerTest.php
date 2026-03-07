c3c2933b2bc246e631599b268ee1f02fe4e58dfa
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-07 11:36:08 +0300

    Create tests for InstallationObjectController

---
+++ tests/Feature/InstallationObjectControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Models\InstallationObject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+
+uses(RefreshDatabase::class);
+
+it('can view the list of installation objects', function () {
+    InstallationObject::factory()
+        ->count(5)
+        ->create();
+
+    $response = $this->get(route('installation-objects.index'));
+
+    $response->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('InstallationObject/Index')
+            ->has('installationObjects', 5, fn (Assert $page) => $page
+                ->has('id')
+                ->has('name')
+                ->has('address')
+                ->whereType('id', 'integer')
+                ->whereType('name', 'string')
+                ->whereType('address', 'string')
+            )
+        );
+});
+
+it('can view the installation object', function () {
+    InstallationObject::factory()
+        ->hasMeters(3)
+        ->hasUspds(3)
+        ->create();
+
+    $installationObject = InstallationObject::with(['meters', 'uspds'])
+        ->first();
+
+    $response = $this->get(route('installation-objects.show', $installationObject));
+
+    $response->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('InstallationObject/Show')
+            ->has('id')
+            ->has('name')
+            ->where('id', $installationObject->id)
+            ->where('name', $installationObject->name)
+            ->whereType('id', 'integer')
+            ->whereType('name', 'string')
+            ->has('meters', 3, fn (Assert $meter) => $meter
+                ->has('id')
+                ->has('model')
+                ->has('serialNumber')
+                ->where('id', $installationObject->meters->first()->id)
+                ->where('model', $installationObject->meters->first()->model)
+                ->where('serialNumber', $installationObject->meters->first()->serial_number)
+                ->whereType('id', 'integer')
+                ->whereType('model', 'string')
+                ->whereType('serialNumber', 'string'))
+            ->has('uspds', 3, fn (Assert $uspd) => $uspd
+                ->has('id')
+                ->has('model')
+                ->has('serialNumber')
+                ->where('id', $installationObject->uspds->first()->id)
+                ->where('model', $installationObject->uspds->first()->model)
+                ->where('serialNumber', $installationObject->uspds->first()->serial_number)
+                ->whereType('id', 'integer')
+                ->whereType('model', 'string')
+                ->whereType('serialNumber', 'integer'))
+        );
+});
