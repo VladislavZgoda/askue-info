f41d8f182a286a47100c67fc9324cf8992c156d6
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-09 16:44:16 +0300

    Refactor the Edit Page UI

--- resources/js/pages/InstallationObject/Edit.tsx
+++ resources/js/pages/InstallationObject/Edit.tsx
@@ -1,10 +1,11 @@
 import { Form, Link } from '@inertiajs/react';
+import { FactoryIcon, ListStart, MapPinHouseIcon, MoveLeft, RotateCcwIcon, SaveIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { ButtonGroup } from '@/components/ui/button-group';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Field, FieldDescription, FieldError, FieldLabel } from '@/components/ui/field';
-import { Input } from '@/components/ui/input';
+import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
 import { Spinner } from '@/components/ui/spinner';
 import { index, show, update } from '@/routes/installation-objects';
 import type { InstallationObject } from '@/types';
@@ -19,38 +20,40 @@ export default function Edit({ id, name, address }: InstallationObject) {
                             <CardTitle>Редактировать объект установки</CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <Field data-invalid={errors.name}>
+                            <Field data-invalid={errors.name ? true : false}>
                                 <FieldLabel htmlFor="name">Наименование</FieldLabel>
-                                <Input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    defaultValue={name}
-                                    data-invalid={errors.name}
-                                />
+                                <InputGroup>
+                                    <InputGroupInput type="text" id="name" name="name" defaultValue={name} />
+                                    <InputGroupAddon align="inline-start">
+                                        <FactoryIcon />
+                                    </InputGroupAddon>
+                                </InputGroup>
                                 <FieldDescription>Выберете уникальное наименование.</FieldDescription>
                                 {errors.name && <FieldError>{errors.name}</FieldError>}
                             </Field>
-                            <Field data-invalid={errors.address} className="mt-2">
+                            <Field data-invalid={errors.address ? true : false} className="mt-2">
                                 <FieldLabel htmlFor="address">Адрес</FieldLabel>
-                                <Input
-                                    type="text"
-                                    id="address"
-                                    name="address"
-                                    defaultValue={address}
-                                    data-invalid={errors.address}
-                                />
+                                <InputGroup>
+                                    <InputGroupInput type="text" id="address" name="address" defaultValue={address} />
+                                    <InputGroupAddon align="inline-start">
+                                        <MapPinHouseIcon />
+                                    </InputGroupAddon>
+                                </InputGroup>
                                 {errors.address && <FieldError>{errors.address}</FieldError>}
                             </Field>
                         </CardContent>
                         <CardFooter>
                             <Field orientation="horizontal">
                                 <Button disabled={processing}>
-                                    {processing && <Spinner data-icon="inline-start" />}
+                                    {processing ? (
+                                        <Spinner data-icon="inline-start" />
+                                    ) : (
+                                        <SaveIcon data-icon="inline-start" />
+                                    )}
                                     {processing ? 'Подождите' : 'Изменить'}
                                 </Button>
                                 <Button type="button" variant="outline" onClick={() => reset()} disabled={processing}>
-                                    Сбросить
+                                    <RotateCcwIcon data-icon="inline-start" /> Сбросить
                                 </Button>
                             </Field>
                         </CardFooter>
@@ -61,12 +64,12 @@ export default function Edit({ id, name, address }: InstallationObject) {
             <ButtonGroup orientation="vertical" className="mx-auto mt-2 w-full max-w-xs rounded-md shadow-sm">
                 <Button asChild variant="outline">
                     <Link href={index()} prefetch>
-                        Список объектов
+                        <ListStart data-icon="inline-start" /> Список объектов
                     </Link>
                 </Button>
                 <Button asChild variant="outline">
                     <Link href={show(id)} prefetch>
-                        Назад
+                        <MoveLeft data-icon="inline-start" /> Назад
                     </Link>
                 </Button>
             </ButtonGroup>
