448943c7dc5be9afe0c08ecd145a9368b59a0dcb
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-18 15:42:33 +0300

    Add a navaigation menu to the RootLayout

--- resources/js/layouts/RootLayout.tsx
+++ resources/js/layouts/RootLayout.tsx
@@ -1,7 +1,15 @@
-import { router } from '@inertiajs/react';
+import { Link, router } from '@inertiajs/react';
 import type React from 'react';
 import { useEffect } from 'react';
 import { toast, Toaster } from 'sonner';
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     useEffect(() => {
@@ -16,6 +24,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
     return (
         <>
+            <NavigationMenu className="mx-auto mt-1.5 w-full max-w-xs justify-start">
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                            <Link href="/">Главная</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
             {children}
             <Toaster />
         </>
