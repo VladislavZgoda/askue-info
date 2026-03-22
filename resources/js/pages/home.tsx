bac546ce1103e01f8a5864812819cf6475ec6e48
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-22 16:58:21 +0300

    Add link to home

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,11 +1,23 @@
+import { Link } from '@inertiajs/react';
+import { ListStart } from 'lucide-react';
+
+import { index } from '@/actions/App/Http/Controllers/MeterController';
+import { Button } from '@/components/ui/button';
 import ViewInstallationObjectsButton from '@/components/ViewInstallationObjectsButton';
 
 export default function Home() {
     return (
-        <div className="mt-5 flex">
+        <div className="mt-5 flex flex-col gap-2">
             <ViewInstallationObjectsButton className="mx-auto w-full max-w-xs">
                 Просмотр объектов установки
             </ViewInstallationObjectsButton>
+
+            <Button asChild variant="outline" className="mx-auto w-full max-w-xs">
+                <Link href={index()}>
+                    <ListStart data-icon="inline-start" />
+                    Просмотр приборов учёта
+                </Link>
+            </Button>
         </div>
     );
 }
