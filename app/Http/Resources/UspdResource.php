f7cb6cff9289862eb75cb1ddb5cb0e670a0ef310
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-22 14:58:43 +0300

    Create UspdResource

---
+++ app/Http/Resources/UspdResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UspdResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'model' => $this->model,
+            'serial_number' => $this->serial_number,
+        ];
+    }
+}
