4146e13c3348b4089585fad8efa135bdec7c7d40
VladislavZgoda <www.vlad78@yandex.ru>
2026-03-24 17:07:16 +0300

    Add test to HomePageTest

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\Models\InstallationObject;
+use App\Models\Meter;
 
 it('visits the Home page', function () {
     $page = visit('/')->on()->mobile();
 
-    $page->assertSeeLink('Просмотр объектов установки');
+    $page->assertSeeLink('Просмотр объектов установки')
+        ->assertSeeLink('Просмотр приборов учёта');
 });
 
-it('navigates from the Home page to installation-objects.show using the link', function () {
+it('navigates from the Home page to installation-objects.index using the link', function () {
     $installationObject = InstallationObject::factory()->create();
 
     $page = visit('/')->on()->mobile();
@@ -17,4 +19,15 @@ it('navigates from the Home page to installation-objects.show using the link', f
         ->assertUrlIs(route('installation-objects.index'))
         ->assertSee($installationObject->name)
         ->assertSee($installationObject->address);
+});
+
+it('navigates from the Home page to meters.index using the link', function () {
+    $meter = Meter::factory()->create();
+
+    $page = visit('/')->on()->mobile();
+
+    $page->click('Просмотр приборов учёта')
+        ->assertUrlIs(route('meters.index'))
+        ->assertSee($meter->model)
+        ->assertSee($meter->serial_number);
 });
