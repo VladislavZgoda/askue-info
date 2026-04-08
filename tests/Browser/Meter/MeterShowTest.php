52585ea1c6301fd024ee6f17cc594c0ae930de81
zgodavg <www.vlad78@yandex.ru>
2026-04-08 11:56:12 +0300

    test: add the browser tests for MeterController

---
+++ tests/Browser/Meter/MeterShowTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Meter;
+
+it('renders the page with :dataset', function (Meter $meter) {
+    $meter->load('simCards');
+
+    $url = route('meters.show', $meter);
+    $page = visit($url)->on()->mobile();
+
+    $page->assertUrlIs($url)
+        ->assertSee("$meter->model №$meter->serial_number")
+        ->assertSeeLink('Просмотр приборов учёта')
+        ->assertSeeLink('Добавить сим-карту')
+        ->assertNoJavaScriptErrors();
+
+    foreach ($meter->simCards as $simCard) {
+        $page->assertSee($simCard->operator)
+            ->assertSee($simCard->number);
+
+        if ($simCard?->ip) {
+            $page->assertSee($simCard->ip);
+        }
+    }
+})->with([
+    'one sim card' => fn () => Meter::factory()->hasSimCards(1)->create(),
+    'two sim cards' => fn () => Meter::factory()->hasSimCards(2)->create(),
+    'zero sim cards' => fn () => Meter::factory()->create(),
+]);
+
+it('navigates from the Show page to the Index page using the link "Просмотр приборов учёта"', function () {
+    $meter = Meter::factory()->create();
+
+    $page = visit(route('meters.show', $meter))
+        ->on()
+        ->mobile();
+
+    $page->click('Просмотр приборов учёта')
+        ->assertUrlIs(route('meters.index'))
+        ->assertSee($meter->model)
+        ->assertSee($meter->serial_number)
+        ->assertNoJavaScriptErrors();
+});
