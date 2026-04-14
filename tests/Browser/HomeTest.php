93885a7ccc1462679805fda94fdedf2a93d668ba
zgodavg <www.vlad78@yandex.ru>
2026-04-14 14:40:43 +0300

    test: add the browser tests for home page

--- tests/Browser/HomeTest.php
+++ tests/Browser/HomeTest.php
@@ -2,12 +2,15 @@
 
 use App\Models\InstallationObject;
 use App\Models\Meter;
+use App\Models\SimCard;
 
 it('visits the Home page', function () {
     $page = visit('/')->on()->mobile();
 
     $page->assertSeeLink('Просмотр объектов установки')
-        ->assertSeeLink('Просмотр приборов учёта');
+        ->assertSeeLink('Просмотр приборов учёта')
+        ->assertSeeLink('Просмотр сим-карт')
+        ->assertNoJavaScriptErrors();
 });
 
 it('navigates from the Home page to installation-objects.index using the link', function () {
@@ -18,7 +21,8 @@ it('navigates from the Home page to installation-objects.index using the link', 
     $page->click('Просмотр объектов установки')
         ->assertUrlIs(route('installation-objects.index'))
         ->assertSee($installationObject->name)
-        ->assertSee($installationObject->address);
+        ->assertSee($installationObject->address)
+        ->assertNoJavaScriptErrors();
 });
 
 it('navigates from the Home page to meters.index using the link', function () {
@@ -29,5 +33,18 @@ it('navigates from the Home page to meters.index using the link', function () {
     $page->click('Просмотр приборов учёта')
         ->assertUrlIs(route('meters.index'))
         ->assertSee($meter->model)
-        ->assertSee($meter->serial_number);
+        ->assertSee($meter->serial_number)
+        ->assertNoJavaScriptErrors();
+});
+
+it('navigates from the Home page to sim-cards.index using the link', function () {
+    $simCard = SimCard::factory()->create();
+
+    $page = visit('/')->on()->mobile();
+
+    $page->click('Просмотр сим-карт')
+        ->assertUrlIs(route('sim-cards.index'))
+        ->assertSee($simCard->operator)
+        ->assertSee($simCard->number)
+        ->assertNoJavaScriptErrors();
 });
