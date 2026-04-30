fcc1c31da650c90dfba243467d304562cfa71b2d
zgodavg <www.vlad78@yandex.ru>
2026-04-30 10:37:54 +0300

    feat: add UpdateUspdRequest

---
+++ app/Http/Requests/UpdateUspdRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUspdRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'model' => [
+                'required',
+                Rule::in([
+                    'RTR8A.LRsGE-1-1-RUFG',
+                    'RTR8A.LRsGE-2-1-RUFG',
+                    'RTR8A.LGE-2-2-RUF',
+                    'RTR58A.LG-1-1',
+                    'RTR58A.LG-2-1',
+                ]),
+            ],
+            'serial_number' => [
+                'required',
+                'integer',
+                'digits:7',
+                Rule::unique('uspds')->ignore($this->route('uspd')),
+            ],
+            'lan_ip' => [
+                'nullable',
+                'ipv4',
+                Rule::unless(
+                    fn () => $this->input('lan_ip') === '192.168.0.100',
+                    Rule::unique('uspds', 'lan_ip')->ignore($this->route('uspd')),
+                ),
+            ],
+        ];
+    }
+}
